0dc9486a87dfe31d8c17219afb3edab02b7e4ca2
Simon Christ <SimonChrist@gmx.de>
2025-08-27 16:22:41 +0200

    implementation using a matrix and an enum

--- Pokemon.jl
+++ Pokemon.jl
@@ -1,119 +1,43 @@
-#definning structures
-abstract type AbstractType end
-struct Normal <: AbstractType end
-struct Fire <: AbstractType end
-struct Water <: AbstractType end
-struct Electric <: AbstractType end
-struct Grass <: AbstractType end
-struct Ice <: AbstractType end
-struct Fighting <: AbstractType end
-struct Poison <: AbstractType end
-struct Ground <: AbstractType end
+effectivity_matrix = [ 1 1 1 1 1 1 1 1 1;
+                       1 1/2 1/2 1 2 2 1 1 1;
+                       1 2 1/2 1 1/2 1 1 1 2;
+                       1 1 2 1/2 1/2 1 1 1 0;
+                       1 1/2 2 1 1/2 1 1 1/2 2;
+                       1 1/2 1/2 1 2 1/2 1 1 2;
+                       2 1 1 1 1 2 1 1/2 1;
+                       1 1 1 1 2 1 1 1/2 1/2;
+                       1 2 1 2 1/2 1 1 2 1]
 
-#definning const
-const no_effect = 0
-const not_very_effective = 1/2
-const normally_effective = 1
-const super_effective = 2
-
-
-#Defining eff
-#default if not defined = normally_effective (most cases) 
-eff(atk::AbstractType, def::AbstractType) = normally_effective
-
-#general case in which self aginst self = not_very_effective (probably there is a way to generalize this)
-eff(atk::T, def::T) where T<:AbstractType = not_very_effective
-
-#excpection for Normal, Fighting, Ground
-eff(atk::Normal, def::Normal) = normally_effective
-eff(atk::Fighting, def::Fighting) = normally_effective
-eff(atk::Ground, def::Ground) = normally_effective
-
-#only instance = no_effect
-eff(atk::Electric, def::Ground) = no_effect
-
-#Excpetions
-#Fire(atk)
-eff(atk::Fire, def::Grass) = super_effective
-eff(atk::Fire, def::Ice) = super_effective
-eff(atk::Fire, def::Water) = not_very_effective
-
-#Water(atk)
-eff(atk::Water, def::Fire) = super_effective
-eff(atk::Water, def::Ground) = super_effective
-eff(atk::Water, def::Grass) = not_very_effective
-
-#Electric(atk)
-eff(atk::Electric, def::Water) = super_effective
-eff(atk::Electric, def::Grass) = not_very_effective
-
-#Grass(atk)
-eff(atk::Grass, def::Water) = super_effective
-eff(atk::Grass, def::Ground) = super_effective
-eff(atk::Grass, def::Fire) = not_very_effective
-eff(atk::Grass, def::Poison) = not_very_effective
-
-#Ice(atk)
-eff(atk::Ice, def::Grass) = super_effective
-eff(atk::Ice, def::Ground) = super_effective
-eff(atk::Ice, def::Fire) = not_very_effective
-eff(atk::Ice, def::Water) = not_very_effective
-
-#Fighting(atk)
-eff(atk::Fighting, def::Ice) = super_effective
-eff(atk::Fighting, def::Poison) = not_very_effective
-eff(atk::Fighting, def::Normal) = super_effective
+@enum PokeType begin
+  Normal = 1
+  Fire
+  Water
+  Electric
+  Grass
+  Ice
+  Fighting
+  Poison
+  Ground
+end
 
 
-#Poison(atk)
-eff(atk::Poison, def::Grass) = super_effective
-eff(atk::Poison, def::Ground) = not_very_effective
-
-#Ground(atk)
-eff(atk::Ground, def::Fire) = super_effective
-eff(atk::Ground, def::Electric) = super_effective
-eff(atk::Ground, def::Poison) = super_effective
-
-#defining eff_string
-function eff_string(atk::AbstractType, def::AbstractType)
-    if eff(atk, def) == super_effective
-        return "The attack was super effective"
-    elseif eff(atk, def) == normally_effective
-        return "The attack was normally effective"
+function eff(atk::PokeType, def::PokeType)
+    effectivity = effectivity_matrix[Int(atk), Int(def)]
+    if effectivity == 1
+        return "was normally effective."
+    elseif  effectivity == 2
+        return "was super effective!!"
+    elseif effectivity == 1/2
+        return "was not very effective 🙁"
     else
-        return "The attack was not effective..."
+        return "had no effect.."
     end
 end
-
-#define Attack function
-function attack(atk::AbstractType, def::AbstractType)
-    println("The type of the attacking pókemon is: $(typeof(atk))")
-    println("The type of the defending pókemon is: $(typeof(def))")
-    println("Due to the types of the pokemon the attacking pokemon would deal the amount of damage multiplied by $(eff(atk, def))")
-    println(eff_string(atk, def))
+function attack(atk, def)
+    "A $atk Pokemon attacks a $def Pokemon! The attack $(eff(atk, def))"
 end
 
-
-#tests
-eff(Fighting(), Normal())
-eff(Normal(), Water())
-eff(Water(), Water())
-eff(Normal(), Normal())
-eff(Fire(), Electric())
-eff(Electric(), Ground())
-
-
-eff_string(Fighting(), Normal())
-eff_string(Normal(), Water())
-eff_string(Water(), Water())
-eff_string(Normal(), Normal())
-eff_string(Fire(), Electric())
-eff_string(Electric(), Ground())
-
-attack(Fighting(), Normal())
-attack(Normal(), Water())
-attack(Water(), Water())
-attack(Normal(), Normal())
-attack(Fire(), Electric())
-attack(Electric(), Ground())
-
+attack(Normal, Normal)
+attack(Fire, Water)
+attack(Electric, Ground)
+attack(Ice, Grass)
